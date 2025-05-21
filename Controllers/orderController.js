ee6315bea0f7ed52ae01ee488b9f07d59fac05c6
spehesara <spehesara2005@gmail.com>
2025-05-21 12:17:06 +0530

    implement functions

---
+++ Controllers/orderController.js
@@ -0,0 +1,78 @@
+import Order from "../models/Order.js";
+import { isCustomer } from "./userController.js";
+
+
+export async function createOrders(req, res) {
+
+    if (!isCustomer) {
+        res.json({
+            message: "Please login as a Customer to create orders"
+        })
+
+    }
+    try {
+        const latestOrder = await Order.find().sort
+            ({ date: -1 }).limit(1)
+
+        let orderId
+
+        if (latestOrder.length == 0) {
+            orderId = "CBC001"
+
+        } else {
+            const currentOrderId = latestOrder[0].
+                orderId
+            const numberString  = currentOrderId.replace("CBC", "")   
+
+            const number = parseInt(numberString)
+
+            
+
+            const newNumber = (number + 1).toString
+                ().padStart(4, "0");
+
+            orderId = "CBC " + newNumber
+
+        }
+
+        const newOrderData = req.body
+        newOrderData.orderId = orderId
+        newOrderData.email = req.user.email
+
+        const order = new Order(newOrderData)
+
+        await order.save()
+
+        res.json({
+meesage: "Order Created"
+
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+
+        })
+    }
+
+
+}  
+
+export async function getOrders(req,res) {
+
+    try{
+
+        const orders = await Order.find({email :req.user.email})
+
+        res.json(orders)
+    }catch(error){
+
+res.status(500).json({
+    message: error.message
+})
+
+    }
+
+    
+}
