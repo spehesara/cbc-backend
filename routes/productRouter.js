d90d7c2611e4ca5caea88017dbe8f7cd87d91770
Your NameSithum_pehesara <spehesara2005@gmail.com>
2025-07-24 08:50:04 +0530

    day 17

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 
-import { createProduct, deleteProduct, getProduct, updateProduct } from "../Controllers/productController.js";
+import  { createProduct, deleteProduct, getProduct, updateProduct } from "../Controllers/productController.js";
+import getProductById from "../Controllers/productController.js";
 
 
 
@@ -8,6 +9,8 @@ const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProduct )
+productRouter.get("/:productId",getProductById)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
 export default productRouter;
+
